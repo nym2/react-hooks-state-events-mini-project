7135d2d57b6413d5f42775ca65637a0315e1468c
NymphaPamba <praisepamba@gmail.com>
2024-11-15 00:40:10 +0300

    initial commits

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,29 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // Filter tasks based on selected category
+  const filteredTasks = tasks.filter(task =>
+    selectedCategory === "All" ? true : task.category === selectedCategory
+  );
+
+  // Function to handle adding a new task
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleAddTask} />
+      <TaskList tasks={filteredTasks} setTasks={setTasks} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,26 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, setTasks }) {
+  // Function to remove a task by its text or unique identifier
+  function handleDeleteTask(taskToDelete) {
+    const updatedTasks = tasks.filter(task => task.text !== taskToDelete);
+    setTasks(updatedTasks);
+  }
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task, index) => (
+        <Task
+          key={index}
+          text={task.text}
+          category={task.category}
+          onDeleteTask={handleDeleteTask}
+        />
+      ))}
     </div>
   );
 }
 
 export default TaskList;
+
